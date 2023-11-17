f398df62d205e9bb2fa6b8eee99abe7a3be99dcd
defenghu <defenghu1@hotmail.com>
2023-11-17 13:26:01 +0100

    implementacion de apis

--- appApuestas/src/appApuestas/bd/MainBD.java
+++ appApuestas/src/appApuestas/bd/MainBD.java
@@ -3,6 +3,10 @@ package appApuestas.bd;
 public class MainBD {
 
 	public static void main(String[] args) {
+		CRUDApuestas api = new CRUDAPI();
+		CRUDApuestas api2 = new CRUDAPI2();
 
+		api.crearApuesta(null);
+		api2.crearApuesta(null);
 	}
 }
