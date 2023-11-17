2178a71c8fbf002be2e712b6493de26fdbd00b25
defenghu <defenghu1@hotmail.com>
2023-11-17 12:15:11 +0100

    aprendiendo implementaciones de interfaz

--- appApuestas/src/appApuestas/bd/CRUDApuestas.java
+++ appApuestas/src/appApuestas/bd/CRUDApuestas.java
@@ -1,9 +1,14 @@
 package appApuestas.bd;
 
+import java.util.List;
+
 import appApuestas.Apuestas;
 
 public interface CRUDApuestas {
 	
 	boolean crearApuesta (Apuestas apuesta);
+	
+	List<Apuestas> leerApuestas();
+	
 	
 }
