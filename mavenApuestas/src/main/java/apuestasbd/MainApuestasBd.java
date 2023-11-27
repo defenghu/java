6274b3d9ee00ec2f79864b8d34cca85d46e4bfc2
defenghu <defenghu1@hotmail.com>
2023-11-27 13:45:36 +0100

    update ejercicios

--- mavenApuestas/src/main/java/apuestasbd/MainApuestasBd.java
+++ mavenApuestas/src/main/java/apuestasbd/MainApuestasBd.java
@@ -1,5 +1,21 @@
 package apuestasbd;
 
 public class MainApuestasBd {
-
+	public static void main(String[] args) {
+		int opcion = Pantalla.menuPantallaInicio();
+		
+		switch (opcion) {
+		case 1:
+			//acceso
+			break;
+		case 2;
+			//registro
+			break;
+		case 3;
+			//salir
+		default:
+			System.out.println("opcion invalida");
+			break;
+		}
+	}
 }
--- mavenApuestas/src/main/java/apuestasbd/modelo/Apuesta.java
+++ mavenApuestas/src/main/java/apuestasbd/modelo/Apuesta.java
@@ -1,5 +1,47 @@
 package apuestasbd.modelo;
 
 public class Apuesta {
-
+	private float valor;
+	private int golesVisitante;
+	private int idpartido;
+	private int golesLocal;
+	
+	
+	public Apuesta() {
+		super();
+	}
+	
+	public Apuesta(int valor, int golesLocales, int golesVisitante, int idpartido) {
+		super();
+		this.valor = valor;
+		this.golesLocal = golesLocales;
+		this.golesVisitante = golesVisitante;
+		this.idpartido = idpartido;
+		
+	}
+	
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	public int getGolesVisitante() {
+		return golesVisitante;
+	}
+	public void setGolesVisitante(int golesVisitante) {
+		this.golesVisitante = golesVisitante;
+	}
+	public int getIdpartido() {
+		return idpartido;
+	}
+	public void setIdpartido(int idpartido) {
+		this.idpartido = idpartido;
+	}
+	public int getGolesLocal() {
+		return golesLocal;
+	}
+	public void setGolesLocal(int golesLocal) {
+		this.golesLocal = golesLocal;
+	}
 }
