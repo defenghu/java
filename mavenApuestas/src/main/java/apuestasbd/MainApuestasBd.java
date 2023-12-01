a45ee6f67874093a3208e64e65a6bc5f70b85a3b
defenghu <defenghu1@hotmail.com>
2023-12-01 13:48:44 +0100

    update 1 de Diciembre

--- mavenApuestas/src/main/java/apuestasbd/MainApuestasBd.java
+++ mavenApuestas/src/main/java/apuestasbd/MainApuestasBd.java
@@ -1,5 +1,12 @@
 package apuestasbd;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import apuestasbd.modelo.Equipo;
 
 public class MainApuestasBd {
 	
@@ -39,4 +46,27 @@ public class MainApuestasBd {
 			}	
 		}
 	}
+	
+	
+	
+	
+	public static void generarPartidos() {
+		
+		try (Connection conexion = BaseDatos.obtenerConexion();){
+			  conexion.setAutoCommit(false);        
+			  PreparedStatement prepStmt = conexion.prepareStatement(    
+			    "insert into equipos(idequipos, nombre) values (?,?)");
+			  List<Equipo> lEquipos = new ArrayList<>();	
+			lEquipos = Equipo.cargarListaEquipos("src/main/resources/equipos.txt");
+			  for (int i = 0; i < lEquipos.size(); i++) {
+				  Equipo e = lEquipos.get(i);
+				    prepStmt.setInt(1, e.getIdequipo());       
+				    prepStmt.setString(2,e.getNombre());
+				    prepStmt.executeUpdate();
+			  }
+		} catch(Exception e) {
+			  // process BatchUpdateException
+		} 
+	}
+	
 }
--- mavenApuestas/src/main/java/apuestasbd/modelo/Equipo.java
+++ mavenApuestas/src/main/java/apuestasbd/modelo/Equipo.java
@@ -1,5 +1,92 @@
 package apuestasbd.modelo;
 
-public class Equipo {
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Equipo implements Comparable<Equipo> {
+
+	private int idequipo;
+	private String nombre;
+
+	public int getIdequipo() {
+		return idequipo;
+	}
+
+	public void setIdequipo(int idequipo) {
+		this.idequipo = idequipo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Equipo(int idequipo, String nombre) {
+		super();
+		this.idequipo = idequipo;
+		this.nombre = nombre;
+	}
+
+	public Equipo(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public Equipo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Equipo [idequipo=" + idequipo + ", nombre=" + nombre + "]\n";
+	}
+
+	@Override
+	public int compareTo(Equipo o) {
+		int resultado = 0;
+
+		resultado = this.nombre.compareTo(o.nombre);
+
+		return resultado;
+	}
+
+	public static List<Equipo> cargarListaEquipos(String ruta) {
+		List<Equipo> lEquipos = null;
+
+		try {
+			Path pathEquipos = Path.of(ruta);
+
+			List<String> lineas = Files.readAllLines(pathEquipos);
+
+			lEquipos = new ArrayList<Equipo>(lineas.size());
+
+			for (String linea : lineas) {
+				if (!linea.startsWith("*")) {
+					Equipo equipo = new Equipo(linea);
+					lEquipos.add(equipo);
+				}
+			}
+
+			Collections.sort(lEquipos);
+
+			System.out.println(lEquipos.toString());
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return lEquipos;
+
+	}
+	
+	public static void main(String[] args) {
+		System.out.println((Partido.generarPartidos(cargarListaEquipos("src/main/resources/equipos.txt"))).toString());
+		
+	}
 }
